57f0cc918f75bd52c3ec66e93356d3639c6881cb
Md Ridoy Hossain <inception.helps@gmail.com>
2023-03-03 19:08:48 +0600

    version 0.0

---
+++ main.py
@@ -0,0 +1,27 @@
+import pikepdf
+import time
+from colorama import Fore
+from string import *
+from itertools import product
+
+value = ascii_letters + digits + punctuation
+
+count = 0
+start_time = time.time()
+for i in range(4,3,-1):
+    for j in product(value, repeat = i):
+        count += 1
+        word = "".join(j)
+        print(Fore.RED+ f"\r{count} password trying but didn't match: {word}",end="")
+        try:
+            pikepdf.open("Your_PDF_File_Name.pdf", password=str(word))
+            end_time = time.time()
+            print("\n")
+            print(Fore.GREEN+f"Password found in {str(end_time-start_time)[:4]} second \nPassword is: ",end= "")
+            print(Fore.BLUE+f"{word}")
+            break
+        except:
+            continue
+
+
+print(Fore.LIGHTYELLOW_EX+ "Thank you")
